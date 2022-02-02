7f76fdcf279009d928ab437ce0f85e8363342619
Saran33 <36115026+Saran33@users.noreply.github.com>
2022-02-02 17:46:33 +0400

    Update

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except IOError:
 setup(
     name='alpha_vantage',
     version='2.3.2',
-    author='Romel J. Torres', 'Saran Connolly'
+    author='Romel J. Torres'
     author_email='romel.torres@gmail.com',
     license='MIT',
     description='Python module to get stock data from the Alpha Vantage Api',
